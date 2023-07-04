aecab989ac06609a3e5c4709e3f618bf3dd45760
calebgrae09 <103107959+calebgrae09@users.noreply.github.com>
2023-07-04 18:01:36 +0000

    Add files via upload

---
+++ MyVotingApp/app/src/androidTest/java/com/example/myvotingapp/RegisterActivity.java
@@ -0,0 +1,4 @@
+package com.example.myvotingapp;
+
+public class RegisterActivity {
+}
---
+++ MyVotingApp/app/src/main/java/com/example/myvotingapp/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.myvotingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView btn = findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
